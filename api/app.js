0f67c4f46f6c06777f79fd855b482e427ec89aaf
Ali Cebe <alicebe86@gmail.com>
2024-10-10 11:31:07 -0400

    Vercel.json added

--- api/app.js
+++ api/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const connectDB = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
-const employeeRoutes = require('./routes/employeeRoutes');
+const connectDB = require('../config/db');
+const userRoutes = require('../routes/userRoutes');
+const employeeRoutes = require('../routes/employeeRoutes');
 const cors = require('cors');
 
 const app = express();
 connectDB();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
+
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/emp', employeeRoutes);
 
--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/101205359_comp3123_assignment1', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb://localhost:27017/101205359_comp3123_assignment1');
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
