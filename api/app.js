2d08d2240b4e845841aa5557bedd88ab74df1617
Ali Cebe <alicebe86@gmail.com>
2024-10-10 12:00:25 -0400

    Removed nodemon for Vercel deployment

--- api/app.js
+++ api/app.js
@@ -1,20 +1,27 @@
 const express = require('express');
-const connectDB = require('../config/db');
-const userRoutes = require('../routes/userRoutes');
-const employeeRoutes = require('../routes/employeeRoutes');
-const cors = require('cors');
 const path = require('path');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const employeeRoutes = require('./routes/employeeRoutes');
+const cors = require('cors');
 
 const app = express();
 connectDB();
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/emp', employeeRoutes);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
