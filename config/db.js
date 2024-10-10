6e28bb591e53525c0acb8f735de70e005e331981
Ali Cebe <alicebe86@gmail.com>
2024-10-10 11:37:54 -0400

    db.js editted

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/101205359_comp3123_assignment1');
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
